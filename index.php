06d8993c683617159ae27ca85be7fb440891685e
Navarr Barnier <navarr@gtaero.net>
2011-03-11 03:12:59 +0000

    Changes from Barebones

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once("settings.php");
 <form onsubmit="outlookConnect();return false;">
 <p>
 <label for="user"><acronym title="Google Voice">GV</acronym> Login:</label> <input type="text" id="user" name="UserID" /><br />
@@ -8,6 +9,6 @@
 	function outlookConnect()
 	{
 		UserID = document.getElementById('user').value;
-		document.location = 'oms:https://localhost.gtaero.net/oms/service.php?UserID=' + UserID;
+		document.location = 'oms:<?php echo Settings::SERVICE_BASE; ?>/service.php?UserID=' + UserID;
 	}
 </script>
--- lib/googleVoice.php
+++ lib/googleVoice.php
@@ -1,60 +1,88 @@
 <?php
 class GoogleVoice
 {
-	public $username;
-	public $password;
+        public $username;
+        public $password;
 
-	protected $lastURL;
-	protected $crumb;
+        private $lastURL;
+        private $crumb;
 
-	public function __construct($username, $password)
-	{
-		$this->username = $username;
-		$this->password = $password;
-		$this->login();
-	}
-	// Login to Google Voice
-	private function login()
-	{
-		$html = $this->curl('http://www.google.com/voice/m');
+        public $useragent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.6 (KHTML, like Gecko) Chrome/7.0.503.0 Safari/534.6";
 
-		$action = $this->match('!<form.*?action="(.*?)"!ms', $html, 1);
+        public $debug = FALSE;
 
-		preg_match_all('!<input.*?type="hidden".*?name="(.*?)".*?value="(.*?)"!ms', $html, $hidden);
+        public function __construct($username,$password)
+        {
+        	$this->username = $username;
+                $this->password = $password;                
+                $this->login();
+        }
+        // Login to Google Voice
+        private function login()
+        {
+                $html = $this->curl("http://www.google.com/voice/m");
 
-		$post = "Email={$this->username}&Passwd={$this->password}";
-		for ($i = 0; $i < count($hidden[0]); $i++)
-		$post .= '&'.$hidden[1][$i].'='.urlencode($hidden[2][$i]);
+                $dom = new DOMDocument();
+                $dom->loadHTML($html);
+                $form = $dom->getElementById("gaia_loginform");
+                $inputList = $form->getElementsByTagName("input");
+                $inputAmt  = $inputList->length;
+                $inputs = array();
 
-		$html = $this->curl($action, $this->lastURL, $post);
 
-		$this->crumb = urlencode($this->match('!<input.*?name="_rnr_se".*?value="(.*?)"!ms', $html, 1));
-		
-		if(!$this->crumb)
-			{ throw new Exception("Unable to Log In to Google Voice"); }
-	}
-	// Connect $you to $them. Takes two 10 digit US phone numbers.
-	public function call($you, $them)
-	{
-		$you = preg_replace('/[^0-9]/', '', $you);
-		$them = preg_replace('/[^0-9]/', '', $them);
+                for($i = 0; $i < $inputAmt;$i++)
+                {
+                        $inputs[$inputList->item($i)->getAttribute('name')] = $inputList->item($i)->getAttribute('value');
+                }
+                $action = $form->getAttribute('action');
+                print($action);
+                print_r($inputs);
+                $inputs["Email"] = $this->username;
+                $inputs["Passwd"] = $this->password;
+                $post = http_build_query($inputs);
 
-		$crumb = $this->crumb;
+                $html = $this->curl($action, $this->lastURL, $post);
 
-		$post = "_rnr_se=$crumb&phone=$you&number=$them&call=Call";
-		$html = $this->curl("https://www.google.com/voice/m/sendcall", $this->lastURL, $post);
-	}
-	public function sms($them, $smtxt)
-	{
-		$them = preg_replace('/[^0-9]/', '', $them);
+                $this->crumb = urlencode($this->match('!<input.*?name="_rnr_se".*?value="(.*?)"!ms', $html, 1));
+                if(!$this->crumb) { throw new Exception("Couldn't get the Crumb!"); }
+        }
+        // Connect $you to $them. Takes two 10 digit US phone numbers.
+        public function call($you, $them)
+        {
+                $you = preg_replace('/[^0-9]/', '', $you);
+                $them = preg_replace('/[^0-9]/', '', $them);
 
-		$crumb = $this->crumb;
+                $crumb = $this->crumb;
 
-		$post = "_rnr_se=$crumb&number=$them&smstext=$smtxt&submit=Send";
-		$html = $this->curl("https://www.google.com/voice/m/sendsms", $this->lastURL, $post);
-		return $html;
-	}
-	public function get_number()
+                $post = "_rnr_se=$crumb&number=$them&call=Call";
+                $html = $this->curl("https://www.google.com/voice/m/callsms", $this->lastURL, $post);
+
+                preg_match_all('!<input.*?type="hidden".*?name="(.*?)".*?value="(.*?)"!ms', $html, $hidden);
+                $post = '';
+                for ($i = 0; $i < count($hidden[0]); $i++)
+                $post .= '&'.$hidden[1][$i].'='.urlencode($hidden[2][$i]);
+                $post .= "&phone=+1$you&Call=";
+
+                $html = $this->curl("https://www.google.com/voice/m/sendcall", $this->lastURL, $post);
+        }
+        public function sms($them, $smtxt)
+        {
+                $them = preg_replace('/[^0-9]/', '', $them);
+
+                $crumb = $this->crumb;
+
+                $post = "_rnr_se=$crumb&number=$them&smstext=$smtxt&submit=Send";
+                $html = $this->curl("https://www.google.com/voice/m/sendsms", $this->lastURL, $post);
+
+                preg_match_all('!<input.*?type="hidden".*?name="(.*?)".*?value="(.*?)"!ms', $html, $hidden);
+                $post = '';
+                for ($i = 0; $i < count($hidden[0]); $i++)
+                $post .= '&'.$hidden[1][$i].'='.urlencode($hidden[2][$i]);
+                $post .= "&submit=";
+
+                $html = $this->curl("https://www.google.com/voice/m/sendcall", $this->lastURL, $post);
+        }
+        public function get_number()
 	{
 		$raw = $this->curl("https://www.google.com/voice/m");
 		preg_match("#\<b class=\"ms3\"\>([^<]+)\</b\>#i",$raw,$matches);
@@ -64,49 +92,52 @@ class GoogleVoice
 		"",$matches[1]);
 		return "1".$number;
 	}
-	protected function curl($url, $referer = null, $post = null, $return_header = false)
-	{
-		static $tmpfile;
+        protected function curl($url, $referer = null, $post = null, $return_header = false)
+        {
+                static $tmpfile;
 
-		if (! isset ($tmpfile) || ($tmpfile == ''))
-			{ $tmpfile = tempnam('/tmp', 'FOO'); }
+                if (! isset ($tmpfile) || ($tmpfile == ''))
+                        { $tmpfile = tempnam("\temp", "FOO"); }
 
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $tmpfile);
-		curl_setopt($ch, CURLOPT_COOKIEJAR, $tmpfile);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0); 
-		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (iPhone; U; CPU iPhone OS 2_2_1 like Mac OS X; en-us) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5H11 Safari/525.20");
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_COOKIEFILE, $tmpfile);
+                curl_setopt($ch, CURLOPT_COOKIEJAR, $tmpfile);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                        "Accept" => "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"
+                ));
+                if($this->useragent)
+                        { curl_setopt($ch, CURLOPT_USERAGENT, $this->useragent); }
 
-		if ($referer)
-			{ curl_setopt($ch, CURLOPT_REFERER, $referer); }
+                if ($referer)
+                        { curl_setopt($ch, CURLOPT_REFERER, $referer); }
 
-		if (!is_null($post))
-		{
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		}
+                if (!is_null($post))
+                {
+                        curl_setopt($ch, CURLOPT_POST, true);
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                }
 
-		if ($return_header)
-		{
-			curl_setopt($ch, CURLOPT_HEADER, 1);
-			$html = curl_exec($ch);
-			$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-			$this->lastURL = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-			return substr($html, 0, $header_size);
-		}
-		else
-		{
-			$html = curl_exec($ch);
-			$this->lastURL = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-			if($html === false) { throw new Exception($url." - ".curl_error($ch)); }
-			return $html;
-		}
-	}
-	protected function match($regex, $str, $i = 0)
-	{
-		return preg_match($regex, $str, $match) == 1?$match[$i]:false;
-	}
+                if ($return_header)
+                {
+                        curl_setopt($ch, CURLOPT_HEADER, 1);
+                        $html = curl_exec($ch);
+                        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+                        $this->lastURL = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+                        return substr($html, 0, $header_size);
+                }
+                else
+                {
+                        $html = curl_exec($ch);
+                        $this->lastURL = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+                        return $html;
+                }
+        }
+        protected function match($regex, $str, $i = 0)
+        {
+                return preg_match($regex, $str, $match) == 1?$match[$i]:false;
+        }
 }
